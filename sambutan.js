6179f51ee495c2e5dd2681ea6d32243ec497caae
Praja23 <141608043+Praja23@users.noreply.github.com>
2024-10-05 23:19:50 +0700

    sambutan.js

--- sambutan.js
+++ sambutan.js
@@ -1,19 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Cek apakah pesan selamat datang sudah ditampilkan sebelumnya
   if (!sessionStorage.getItem("welcomeShown")) {
     alert("Selamat datang di Warung Mina Mamah Nada!");
     sessionStorage.setItem("welcomeShown", "true");
   }
 
-  // Menambahkan efek klik pada item menu
+  
   document.querySelectorAll(".menu-item").forEach((item) => {
     item.addEventListener("click", () => {
-      // Navigasi ke halaman item menu tanpa menampilkan popup selamat datang
       window.location.href = item.querySelector("a").getAttribute("href");
     });
   });
 
-  // Menambahkan transisi halus saat navigasi
+ 
   document.querySelectorAll("nav ul li a").forEach((link) => {
     link.addEventListener("click", (event) => {
       event.preventDefault();
